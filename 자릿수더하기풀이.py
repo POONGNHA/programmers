1c3740369b94b3eb834945508748d3ad7fe224a1
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-10-10 15:16:06 +0900

    자릿수더하기풀이

---
+++ 자릿수더하기풀이.py
@@ -0,0 +1,8 @@
+n = 482;
+a = list();
+for i in range(10, 0, -1):
+    if n - (10 ** (i - 1)) >= 0:
+        num = n // 10 ** (i - 1);
+        NumStr = str(num);
+        a.append(int(NumStr[-1]));
+        print(sum(a));
