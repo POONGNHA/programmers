24fa2f8ae70c8493a5da0c6f25ccc564817a63d0
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-10-10 17:13:40 +0900

    22'10'10 코테연습 17시

--- level_1/자연수뒤집어배열로만들기_모범답안.py
+++ level_1/자연수뒤집어배열로만들기_모범답안.py
@@ -1,4 +1,8 @@
 def digit_reverse(n):
-    return list(map(int, reversed(str(n))))
+    return list(map(int, reversed(str(n))));
 
 # reversed()는 리턴값이 비어있기 때문에 list(reversed(a))식으로 처리해주었다.
+# map은 (1,2) 가 있는데 1에 함수, 2에 원하는 값을 넣는다.
+# n = 12345 일때
+# str(n) = "12345"
+#
