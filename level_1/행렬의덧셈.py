e551f8c62f78211a35d67d57f39c39c4655725e1
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-10-17 19:13:40 +0900

    22'10'17 행렬의 덧셈 별거 아닌 문제였지만 하루를 꼬박 써버렸다. [ [ ], [ ], [ ], ... ] 같은 형태의 행렬을 .append와 같은 형태로 만들어준다면 되는 문제였던 것이다.

--- level_1/행렬의덧셈.py
+++ level_1/행렬의덧셈.py
@@ -8,3 +8,16 @@
 # [[1,2],[2,3]]	[[3,4],[5,6]]	[[4,6],[7,9]]
 # [[1],[2]]	[[3],[4]]	[[4],[6]]
 
+arr1 = [[1, 2], [2, 3]]
+arr2 = [[3, 4], [5, 6]]
+
+# for i in range(0, len(arr1)):
+
+# print([[x + y for x, y in zip(arr1[0] , arr2[0])], [a + b for a, b in zip(arr1[1], arr2[1])]])
+
+arr3 = list()
+for i in range(0, len(arr1)):
+    arr3.append([x + y for x, y in zip(arr1[i] , arr2[i])])
+
+# print(arr3)
+return arr3
