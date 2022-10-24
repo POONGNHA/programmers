c4c1c53114a981a506d8d6422ba80e53209548d2
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-10-25 01:25:10 +0900

    22'10'25 유클리드 호제법을 이용해서 다시 풀어보는것이 좋을것같다.

--- level_1/test.py
+++ level_1/test.py
@@ -1,11 +1,14 @@
 def solution(n, m):
+
+    # 최대공약수(GCD) : 유클리드 호제법 사용
     result1 = set([i for i in range(1, n + 1) if n % i == 0])
     result2 = set([i for i in range(1, m + 1) if m % i == 0])
-    result_final1 = max(list(result1.intersection(result2)))
+    GCD = sorted(list(result1.intersection(result2)))[-1]
 
-    result3 = [i for i in range()]
+    # 최소공배수(LCM) : n * m = 최대공약수 * 최소공배수
+    LCM = (n * m) // GCD
 
-    return [result_final1]
 
+    return [GCD,LCM]
 
-print(solution(3, 12))
+print(solution(3,12))
