f829f65c4ce3651024c1e36871ffe6e8eaa58431
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-10-25 22:45:36 +0900

    22'10'25 같은숫자는 싫어 pop을 사용하면 list의 index가 바뀌는것을 염두에 두고 풀어야한다.

--- level_1/test.py
+++ level_1/test.py
@@ -1,1 +1,8 @@
-print(1%1)
+def solution(arr):
+    answer = []
+    for i in range(0,len(arr)-1): # 0~7
+        if arr[i] != arr[i+1]: # 0,1 이 같으면
+            answer.append(i)
+    return answer
+
+print(solution([1,1,3,3,0,1,1]))
