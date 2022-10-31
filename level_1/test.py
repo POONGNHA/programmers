2cd8479d995e5dc273ed2b65e3efbc9728d04a33
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-10-31 10:48:52 +0900

    22'10'30 햄버거 만들기 이미 만든 햄버거를 배열에서 제외하는 형식의 문제이다. 제외를 해주지 않으면 for 문에서 i는 그대로 증가되는데 그 이전에 있는 1,2,3,1을 합쳐서 하기에 무리가 있다.

--- level_1/test.py
+++ level_1/test.py
@@ -8,12 +8,20 @@
 def solution(ingredient):
     burger = [1, 2, 3, 1]
     cnt = 0
+    flag = True
+    # 버거를 만들고 가운데가 빠지면 앞에거랑 다시 버거를 만들수 있어야함
+
     if len(ingredient) < 4 :
         return 0
     else:
-        for i in range(0, len(ingredient)-3):
-            if ingredient[i:i+4] == burger: # 0, 1, 2, 3,
-                cnt += 1
-        return cnt
+        while flag:
+            flag = False
+            for i in range(0, len(ingredient)-3):
+                if ingredient[i:i+4] == burger: # 0, 1, 2, 3,
+                    del ingredient[i:i+4]
+                    flag = True
+                    cnt += 1
+                    break
+    return cnt
 
-print(solution([1,2,3,1]))
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
