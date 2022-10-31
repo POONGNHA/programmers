3f8b2ac20c56eda7ade0c454335e06c79fd652e1
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-10-31 13:56:57 +0900

    22'10'30 햄버거 만들기 드디어 풀었다. ingredient의 크기가 굉장히 클때 고려해야 시간초과가 발생하지 않는것이 문제였는데 이걸 한 묶음씩 풀어내었다. 모범답안을 보니 하나의 LIST를 더 만들고 그에 따라 비교하는 코드가 훨씬 간단해 보였다.

--- level_1/test.py
+++ level_1/test.py
@@ -1,13 +1,7 @@
-# def solution(ingredient):
-#     answer = 0
-#     return answer
-
-
-
-
 def solution(ingredient):
     burger = [1, 2, 3, 1]
     cnt = 0
+    burger_start = 0
     flag = True
     # 버거를 만들고 가운데가 빠지면 앞에거랑 다시 버거를 만들수 있어야함
 
@@ -16,12 +10,13 @@ def solution(ingredient):
     else:
         while flag:
             flag = False
-            for i in range(0, len(ingredient)-3):
+            for i in range(burger_start, len(ingredient)-3):   # ingredient의 크기가 굉장히 클때 고려해야 시간초과가 발생하지 않는다.
                 if ingredient[i:i+4] == burger: # 0, 1, 2, 3,
                     del ingredient[i:i+4]
                     flag = True
                     cnt += 1
+                    burger_start = i-4
                     break
-    return cnt
+        return cnt
 
 print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
--- level_1/햄버거만들기.py
+++ level_1/햄버거만들기.py
@@ -15,7 +15,22 @@
 def solution(ingredient):
     burger = [1, 2, 3, 1]
     cnt = 0
-    for i in range(0, len(ingredient)-3):
-        if ingredient[i:i+4] == burger: # 0, 1, 2, 3,
-            cnt += 1
-    return cnt
+    burger_start = 0
+    flag = True
+    # 버거를 만들고 가운데가 빠지면 앞에거랑 다시 버거를 만들수 있어야함
+
+    if len(ingredient) < 4 :
+        return 0
+    else:
+        while flag:
+            flag = False
+            for i in range(burger_start, len(ingredient)-3):   # ingredient의 크기가 굉장히 클때 고려해야 시간초과가 발생하지 않는다.
+                if ingredient[i:i+4] == burger: # 0, 1, 2, 3,
+                    del ingredient[i:i+4]
+                    flag = True
+                    cnt += 1
+                    burger_start = i-4
+                    break
+        return cnt
+
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
