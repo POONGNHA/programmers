58da2bc016360e8bc53547e49a2b8e116aa27c7c
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-11-03 22:26:35 +0900

    22'11'03 문제 푸는 방식을 통한 콜라 문제 풀이 콜라문제 풀이완료. 변수를 선정하는것이 참 어렵다 이것때문에 오류가 생겼음
    푸드파이트대회 문제 풀이 완료
    reversed나 reverse는 iterator가 없기때문에
    사용시에 for문이랑 사용해주어야 한다.

--- level_1/test.py
+++ level_1/test.py
@@ -1,10 +1,2 @@
-def solution(a, b, n):
-    cola_possession = 0
-    while n // a >= 1:
-        z = (n // a)
-        x = n % a
-        cola_possession += (n // a) * b
-        n = (n // a) * b + n % a # n = 3일때 오류
-    return cola_possession
-
-print(solution(2,1,20))
+food = [1, 3, 4, 6]
+print(food[1:])
