2d6b432d19b285857d7deeb057c11ef5349af657
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-11-03 14:33:06 +0900

    22'11'03 문제 푸는 방식을 통한 콜라 문제 풀이 # 문제파악 # 유추파악 : 나는 어떻게 할것인가 # 주요단어 영문이름 선정 # 테스트케이스 # 프로그래밍 순서 : 입력 처리 출력 # try-on # 실패시 원인 기록

--- level_1/test.py
+++ level_1/test.py
@@ -1,24 +1,3 @@
-def solution(array):
-    a = list()
-    b = list()
-    for i in array:
-        a.append(array.count(i))
-    for j in a:
-        if j == max(a):
-            b.append(array[a.index(j)])
-    if set(b) == 1:
-        return max(a)
-    else:
-        return -1
-
-
-
-
-
-    # if
-    #     return max(a)
-    # else:
-    #     return -1
-
-
-print(solution([1, 2, 3, 3, 3, 4]))
+def solution(a, b, n):
+    while(n // a != 0):
+        c = n % a
