d7e8e13ae852d22ec3d2572d43352465cda67e64
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-11-03 21:30:20 +0900

    22'11'03 문제 푸는 방식을 통한 콜라 문제 풀이 콜라문제 풀이완료. 변수를 선정하는것이 참 어렵다 이것때문에 오류가 생겼음

--- level_1/test.py
+++ level_1/test.py
@@ -1,3 +1,10 @@
 def solution(a, b, n):
-    while(n // a != 0):
-        c = n % a
+    cola_possession = 0
+    while n // a >= 1:
+        z = (n // a)
+        x = n % a
+        cola_possession += (n // a) * b
+        n = (n // a) * b + n % a # n = 3일때 오류
+    return cola_possession
+
+print(solution(2,1,20))
