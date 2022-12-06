e01821270578c14a54926d11f20deed8060247fe
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2022-12-07 00:36:40 +0900

    22'12'07 겹치는 선분의 거리 진짜 깔끔하게 풀은듯. 기분최고!

---
+++ level_1/문자열 내 마음대로 정렬하기.py
@@ -0,0 +1,27 @@
+# 문제 설명
+# 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때,
+# 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
+# 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면
+# 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+# strings는 길이 1 이상, 50이하인 배열입니다.
+# strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
+# strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
+# 모든 strings의 원소의 길이는 n보다 큽니다.
+# 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+
+# 풀이유추
+# [n:]으로 value를 가지는 dict를 만듬 key값은 index
+#
+
+# 테스트 케이스
+# strings	n	return
+# ["sun", "bed", "car"]	1	["car", "bed", "sun"]
+# ["abce", "abcd", "cdx"]	2	["abcd", "abce", "cdx"]
+
+def solution(strings,n):
+    dict_with_index = {idx: string for idx, string in enumerate(strings)}
+    dict_sorted = sorted(dict_with_index.items(), key=lambda idx: idx[1][n:])
+    return [i for i in dict_sorted[1]]
+
+print(solution(["sun", "bed", "car"],1))
+
--- testNoFunction.py
+++ testNoFunction.py
@@ -1,16 +1,5 @@
-# def solution(n):
-import copy
-
-n = 12
-n_list = [i for i in range(2,n+1)]
-result = []
-
-while(True):
-    compare_list = copy.deepcopy(n_list) # comparelist가 복사된다. comparelist는 소수도 다 지워버림
-    if not compare_list: #compare_list가 비어있다면 중지
-        break
-    else:
-        result.append(min(compare_list)) #
-        for i in compare_list:
-            if i % max(result) == 0:
-                n_list.remove(i)
+strings = ["sun", "bed", "car"]
+n = 1
+dict_with_index = {idx:string for idx,string in enumerate(strings)}
+dict_sorted = sorted(dict_with_index.items(), key = lambda idx : idx[1][n:])
+print(dict_sorted)
